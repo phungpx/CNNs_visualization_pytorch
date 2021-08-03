e8996f2c0a2fa7ec2ddccfbfe61207aa545d99c9
phungpx <phamxuanphungbku@gmail.com>
2021-08-03 11:54:11 +0700

    Update CAM

--- modules/CAM/class_activation_map.py
+++ modules/CAM/class_activation_map.py
@@ -1,24 +1,34 @@
 import cv2
-import math
 import torch
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
+from typing import Tuple, Dict, Optional
 
 import utils
 
 
 class ClassActivationMap(nn.Module):
-    def __init__(self, arch_config, classes, weight_path, image_size, device):
+    def __init__(self, model_config: Dict, classes: Dict, weight_path: str,
+                 image_size: Tuple[int, int] = (224, 224),
+                 mean: Optional[Tuple[float, float, float]] = None,
+                 std: Optional[Tuple[float, float, float]] = None,
+                 device: str = 'cpu'):
         super(ClassActivationMap, self).__init__()
         self.activations = list()
+        self.std = std
+        self.mean = mean
         self.device = device
         self.classes = classes
         self.image_size = image_size
-        self.model = utils.create_instance(arch_config)
+
+        if (self.mean is not None) and (self.std is not None):
+            self.mean = torch.tensor(mean, dtype=torch.float).view(1, 3, 1, 1)
+            self.std = torch.tensor(std, dtype=torch.float).view(1, 3, 1, 1)
+
+        self.model = utils.create_instance(model_config)
         self.model.load_state_dict(torch.load(weight_path, map_location='cpu'))
-        self.model.to(self.device)
-        self.model.eval()
+        self.model.to(self.device).eval()
 
     def _register_forward_hook(self, target_layer):
         assert target_layer in list(self.model._modules.keys()), 'target_layer must be in list of modules of model'
@@ -26,16 +36,7 @@ class ClassActivationMap(nn.Module):
         def hook_feature(module, input, output):
             self.activations.append(output.data)
 
-        def hook_attention_feature(module, input, output):
-            HW, B, C = output.shape
-            H, W = int(math.sqrt(HW)), int(math.sqrt(HW))
-            output = output.reshape(H, W, B, C).permute(2, 3, 0, 1).contiguous()
-            self.activations.append(output)
-
-        if target_layer == 'self_attention':
-            self.model._modules.get(target_layer).register_forward_hook(hook_attention_feature)
-        else:
-            self.model._modules.get(target_layer).register_forward_hook(hook_feature)
+        self.model._modules.get(target_layer).register_forward_hook(hook_feature)
 
     def _get_softmax_weights(self, linear_layer=None):
         if linear_layer:
@@ -45,19 +46,24 @@ class ClassActivationMap(nn.Module):
             softmax_weights = list(self.model.parameters())[-2].data
         return softmax_weights
 
-    def preprocess(self, image):
+    def preprocess(self, image: np.ndarray) -> torch.Tensor:
         sample = cv2.resize(image, dsize=self.image_size)
         sample = torch.from_numpy(sample).to(self.device).to(torch.float)
-        sample = sample.unsqueeze(dim=0).permute(0, 3, 1, 2)
-        sample = (sample - sample.mean()) / sample.std()
+        sample = sample.unsqueeze(dim=0).permute(0, 3, 1, 2).contiguous()
+
+        if (self.mean is not None) and (self.std is not None):
+            sample = (sample - self.mean) / self.std
+        else:
+            sample = (sample - sample.mean()) / sample.std()
+
         return sample
 
-    def process(self, sample):
+    def process(self, sample: torch.Tensor) -> torch.Tensor:
         with torch.no_grad():
             preds = self.model(sample)
             return preds
 
-    def postprocess(self, preds):
+    def postprocess(self, preds: torch.Tensor) -> Tuple[str, float]:
         pred = preds.softmax(dim=1).squeeze(dim=0)
         class_name = self.classes[pred.argmax().item()]
         class_score = pred[pred.argmax()].item()
@@ -65,6 +71,7 @@ class ClassActivationMap(nn.Module):
 
     def CAM(self, sample, class_activation_map_size, target_layer, linear_layer=None):
         self._register_forward_hook(target_layer=target_layer)
+
         with torch.no_grad():
             preds = self.model(sample)
 
@@ -90,10 +97,13 @@ class ClassActivationMap(nn.Module):
 
     def forward(self, image, target_layer='resnet18_conv', linear_layer=None):
         sample = self.preprocess(image)
+
         preds = self.process(sample)
+
         class_name, class_score = self.postprocess(preds)
 
         cam = self.CAM(sample, image.shape[1::-1], target_layer, linear_layer)
+
         cam = (cam * 0.5 + image * 0.5).astype(np.uint8)
 
         return class_name, class_score, cam
